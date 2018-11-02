802594118139a9734377465e026af35008d01638
yanxuewen <yanxuewen325@163.com>
2018-11-02 17:37:25 +0800

    init

--- YMultiThreadDemo/YMultiThreadDemo/ViewController.m
+++ YMultiThreadDemo/YMultiThreadDemo/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "YGCDViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (copy, nonatomic) NSArray *dataArr;
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -16,8 +21,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.navigationItem.title = @"Multi Thread";
+    _dataArr = @[@"GCD"];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _dataArr.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellId = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    cell.textLabel.text = _dataArr[indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if (indexPath.row == 0) {
+        YGCDViewController *gcdVC = [[YGCDViewController alloc] init];
+        [self.navigationController pushViewController:gcdVC animated:YES];
+    }
+}
 
 @end
---
+++ YMultiThreadDemo/YMultiThreadDemo/YGCDViewController.m
@@ -0,0 +1,32 @@
+//
+//  YGCDViewController.m
+//  YMultiThreadDemo
+//
+//  Created by 云谷行 on 2018/11/2.
+//  Copyright © 2018 YXW. All rights reserved.
+//
+
+#import "YGCDViewController.h"
+
+@interface YGCDViewController ()
+
+@end
+
+@implementation YGCDViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
